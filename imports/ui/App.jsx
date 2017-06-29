0e99f8c66c8e43853b344017ef1fea41595a7f36
Adilet Issayev <adilet.issayev@gmail.com>
2017-06-28 21:25:46 -0400

    Added app and task components

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -17,4 +17,17 @@ export default class App extends Component {
       <Task key={task._id} task={task} />
     ));
   }
+  
+  render() {
+    return (
+      <div className="container">
+        <header>
+          <h1> Todo List </h1>
+        </header>
+        <ul>
+          {this.renderTasks()}
+        </ul>
+      </div>
+    )
+  }
 }
